9514ebced56d3610d950cd6188bffe2e55be0840
Chocochips34 <54455668+Chocochips34@users.noreply.github.com>
2019-08-25 17:20:54 +0700

    File
    Gmail peretas

---
+++ geforce now.py
@@ -0,0 +1,10 @@
+import random
+print('This is a free Project by Jonathan B. licenced under the MIT License. [jonathanb.de]')
+
+randomint = random.randint(1, 999999)
+if randomint <= 9:
+	randomint = '0' + str(randomint)
+print(str(randomint) + random.choice(['-abdnwj-bcjwwa-xnfrea', '-brnsam-bsjfsa-bfnrwa', '-mdbrea-btrwqa-nfneas']))
+
+print("done! have fun!")
+input()
